6f8d91c0be706786f90e3b5a2ae52914859c5c6f
Agusprieto05 <162721778+Agusprieto05@users.noreply.github.com>
2024-04-05 12:47:45 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -2,10 +2,10 @@ def check_vowels():
     # Código a implementar utilizando input.
     #input
     
-    name=input("")
-    print("Contiene a: ", "a" in name or "A" in name)
-    print("Contiene e: ", "e" in name or "E" in name)
-    print("Contiene i: ", "i" in name or "I" in name)
-    print("Contiene o: ", "o" in name or "O" in name)
-    print("Contiene u: ", "u" in name or "U" in name) 
+    name=input("").lower()
+    print("Contiene a: ", "a" in name)
+    print("Contiene e: ", "e" in name)
+    print("Contiene i: ", "i" in name)
+    print("Contiene o: ", "o" in name)
+    print("Contiene u: ", "u" in name) 
 
