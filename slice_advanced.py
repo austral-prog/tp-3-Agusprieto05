8f6941d619e5af5655e46ab9662a85c4f99acdff
Agusprieto05 <162721778+Agusprieto05@users.noreply.github.com>
2024-04-05 12:41:31 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -2,5 +2,4 @@ def slice_advanced():
     # Código a implementar utilizando input.
     HW=input("Ingrese cualquier palabra: ")
     print(HW[4::2])
-    HW=input("Ingrese cualquier palabra: ")
-    print(HW[4::2])
+
