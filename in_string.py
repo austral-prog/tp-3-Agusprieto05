a24a6648023354d14042f0c5962971a0e7e58ec1
Agusprieto05 <162721778+Agusprieto05@users.noreply.github.com>
2024-04-07 10:34:59 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,11 +1,11 @@
 def check_vowels():
-    # Código a implementar utilizando input.
-    #input
-    
-    name=input("").lower()
-    print("Contiene a: ", "a" in name)
-    print("Contiene e: ", "e" in name)
-    print("Contiene i: ", "i" in name)
-    print("Contiene o: ", "o" in name)
-    print("Contiene u: ", "u" in name) 
+    name=input("")
+    print()
+    print("Contiene a: ", "a" in name or "A" in name)
+    print("Contiene e: ", "e" in name or "E" in name)
+    print("Contiene i: ", "i" in name or "I" in name)
+    print("Contiene o: ", "o" in name or "O" in name)
+    print("Contiene u: ", "u" in name or "U" in name) 
+check_vowels()
+
 
