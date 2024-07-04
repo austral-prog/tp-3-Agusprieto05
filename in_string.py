83ddd6416a28875886062533d7a09d7cebfe8f5d
Agusprieto05 <162721778+Agusprieto05@users.noreply.github.com>
2024-07-03 23:29:43 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,11 +1,9 @@
 def check_vowels():
-    name=input("")
-    print()
-    print("Contiene a: ", "a" in name or "A" in name)
-    print("Contiene e: ", "e" in name or "E" in name)
-    print("Contiene i: ", "i" in name or "I" in name)
-    print("Contiene o: ", "o" in name or "O" in name)
-    print("Contiene u: ", "u" in name or "U" in name) 
-check_vowels()
+    name = str(input("Nombre: "))
+    print(f"Contiene a: {"a" in name or "A" in name or "á" in name or "Á" in name}")
+    print(f"Contiene e: {"e" in name or "E" in name or "é" in name or "É" in name}")
+    print(f"Contiene i: {"i" in name or "I" in name or "í" in name or "Í" in name}")
+    print(f"Contiene o: {"o" in name or "O" in name}")
+    print(f"Contiene u: {"u" in name or "U" in name}")
 
 
